7bdd7258d1675f7029fad0bb243c390a5819dd40
Md Nurul Hassan <mailme.nurul@gmail.com>
2016-04-30 02:41:29 +0530

    Smart Hospital design and button click event implimented.

---
+++ routes/index.js
@@ -0,0 +1,74 @@
+
+var express = require('express');
+var router = express.Router();
+var app = express();
+
+var os = require('os');
+var url = require('url');
+var mqtt = require('mqtt');
+var http = require('http');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index1', { title1: 'Smart Hospital' });
+
+
+});
+
+router.get('/home', function(req, res, next) {
+  res.render('home', { title1: 'Advance Integrated Tech Lab' });
+});
+
+// Parse
+var mqtt_url = url.parse(process.env.CLOUDMQTT_URL || 'mqtt://localhost:1883');
+var auth = (mqtt_url.auth || ':').split(':');
+
+
+router.post('/ledon', function(req, res) {
+
+  console.log("LED on post Called...");
+
+
+  var client = mqtt.connect(mqtt_url.port, mqtt_url.hostname, {
+    username: auth[0],
+    password: auth[1]
+  });
+
+  var desired = {led : true};
+  client.on('connect', function() {
+    client.publish('/hub/control',desired, function() {
+      client.end();
+      res.writeHead(204, { 'Connection': 'keep-alive' });
+      res.end();
+    });
+  });
+
+  /**/
+});
+
+router.post('/ledoff', function(req, res) {
+
+  console.log("LED off post Called...");
+
+
+  var client = mqtt.connect(mqtt_url.port, mqtt_url.hostname, {
+    username: auth[0],
+    password: auth[1]
+  });
+
+  var desired = {led : false};
+  client.on('connect', function() {
+    client.publish('/hub/control', desired, function() {
+      client.end();
+      res.writeHead(204, { 'Connection': 'keep-alive' });
+      res.end();
+    });
+  });
+
+  /**/
+
+});
+
+
+
+module.exports = router;
---
+++ routes/users.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+
+
+  console.log("Thsi is called from user");
+
+  res.send('respond with a resource');
+});
+
+
+
+module.exports = router;
